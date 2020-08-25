88182a86f6d0dcb3db4dfee90cd94ba2a390508e
Annemarie Thomas <athomas9sa@gmail.com>
2020-08-25 09:32:29 -0400

    changes

--- js/index.js
+++ js/index.js
@@ -1,23 +1,31 @@
 'use strict';
 
 const getTheWeather = document.getElementById('weatherIs');
+const weatherUpdateButton = document.getElementById('submitInfo')
+const myApiKey = '4a66b55f4bb56748d359161740003a5c'
+const myCityName = ("Marietta")
 
+weatherUpdateBotton.addEventListner("click", function(e)) {
+    e.preventDeafault();}
 function weatherIs () {
-    return fetch 
-    ("https://api.openweathermap.org/data/2.5/weather?q=marietta&appid={4a66b55f4bb56748d359161740003a5c}&units=imperial"
-    )
+    let api = 'https://api.openweathermap.org/data/2.5/weather?q=${myCityName}&appid={myApiKey}&units=imperial'
+    return fetch (api)
     .then(function (response){
             return response.json;
     })
     .then(function (data) {
             return data;
     }) 
-}
-        
-(function () {
+    }
+    function () {
     getTheWeather().then(function (response) {
         //console.log("fetch response", response);
         weatherIs.innerHTML = response.main.temp + "°";
     })
-})();
+};
+
+//  °C to °F conversion 
+// function celsiusToFahrenheit(temperature){
+//     // (0 °C × 9/5) + 32 = 32 °F
+//     return (temperature * 9/5) + 32;
     
