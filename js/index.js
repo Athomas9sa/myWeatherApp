6b58994422a0bfecaa77ef30307e91f68dbeadb8
Annemarie Thomas <athomas9sa@gmail.com>
2020-08-25 03:25:54 -0400

    first commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
 'use strict';
 
-const weather = document.getElementById(weather)
+const getTheWeather = document.getElementById('weatherIs');
 
-const getQuote = (category) => {
-const url = 'https://home.openweathermap.org/api_keys=${category}&4a66b55f4bb56748d359161740003a5c'
-
-
-getQuote(url.then(function(response {
-    console.log(response);
-    weather.html = response;
-}))
+function weatherIs () {
+    return fetch 
+    ("https://api.openweathermap.org/data/2.5/weather?q=marietta&appid={4a66b55f4bb56748d359161740003a5c}&units=imperial"
+    )
+    .then(function (response){
+            return response.json;
+    })
+    .then(function (data) {
+            return data;
+    }) 
+}
+        
+(function () {
+    getTheWeather().then(function (response) {
+        //console.log("fetch response", response);
+        weatherIs.innerHTML = response.main.temp + "°";
+    })
+})();
+    
